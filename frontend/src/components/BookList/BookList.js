92796884242c5dc07dd72aa2707d522f23301416
vaspurak avdalian <vaspurakavdalyani@gmail.com>
2024-11-03 16:20:54 +0100

    Delete book by id

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,8 +1,17 @@
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+// import { RiDeleteBin2Line } from "react-icons/ri";
+import { deleteBook } from "../../redux/books/actionCreators";
 import "./BookList.css";
+
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleDeleteBook = (id) => {
+    console.log(deleteBook(id));
+    dispatch(deleteBook(id));
+  };
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -14,6 +23,12 @@ const BookList = () => {
             <li key={book.id}>
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
+              </div>
+              <div className="book-actions">
+                {/* <RiDeleteBin2Line /> */}
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
               </div>
             </li>
           ))}
--- frontend/src/redux/books/reducer.js
+++ frontend/src/redux/books/reducer.js
@@ -6,6 +6,9 @@ const booksReducer = (state = initialState, action) => {
     case a.ADD_BOOK:
       return [...state, action.payload];
 
+    case a.DELETE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+
     default:
       return state;
   }
